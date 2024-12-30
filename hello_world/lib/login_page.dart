2df39578d656e2ac7f235ed16551ea63f66c9df0
gabriel.rodrigues <gabrigues23@gmail.com>
2024-12-30 15:28:35 -0300

    Aula #18 - Navegação entre telas (Manual)

--- hello_world/lib/login_page.dart
+++ hello_world/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -43,6 +44,9 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () {
                     if (email == 'teste@gmail.com' && password == '123') {
                       print('correto!');
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
                     } else {
                       print('login inválido!');
                     }
